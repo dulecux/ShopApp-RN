7356d2d5a40a68a95c1011abf24e8aeb787c65f8
dulecux <dusan.culibrk@live.com>
2021-11-19 00:20:31 +0100

    Added Web Server + Db

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import AppLoading from 'expo-app-loading';
 import * as Font from 'expo-font';
 import { enableScreens } from 'react-native-screens';
+import ReduxThunk from 'redux-thunk';
 
 import productsReducer from './src/store/reducers/products';
 import ShopNavigator from './src/navigation/ShopNavigator';
@@ -18,7 +19,7 @@ const rootReducer = combineReducers({
   orders: ordersReducer
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
 
 const fetchFonts = () => {
 
